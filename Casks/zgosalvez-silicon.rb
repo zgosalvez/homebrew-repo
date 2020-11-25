a37e1661d5631164ac6d9ca0021c1b8654d4d1ed
Zennon Gosalvez <1798166+zgosalvez@users.noreply.github.com>
2020-11-25 19:27:46 +0800

    Upgrade Silicon.app to v1.0.1

--- Casks/zgosalvez-silicon.rb
+++ Casks/zgosalvez-silicon.rb
@@ -1,12 +1,15 @@
 cask "zgosalvez-silicon" do
-  version "1.0.0"
-  sha256 "f06a8ab940db1a28824d765bb2935689b4655ccbde176be80fe4de813ce974e0"
+  version "1.0.1"
+  sha256 "44a2489e3e3b0c11470f1bb83c6bb53676040326f5f688aa9cb27161c9869055"
 
   url "https://github.com/DigiDNA/Silicon/releases/download/#{version}/Silicon.app.zip"
   appcast "https://github.com/DigiDNA/Silicon/releases.atom"
   name "Silicon"
   desc "Identify Intel-only apps"
   homepage "https://github.com/DigiDNA/Silicon"
+
+  auto_updates true
+  depends_on macos: ">= :mojave"
 
   app "Silicon.app"
 
